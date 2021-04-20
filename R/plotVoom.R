255a5073b72c7e461da3acdd90f6216c002e43ba
Gabriel Hoffman <gabrielhoffman@glia.local>
2021-04-19 20:05:00 -0400

    new features

--- R/plotVoom.R
+++ R/plotVoom.R
@@ -24,17 +24,12 @@ plotVoom = function( x, ncol=3){
 	###################################
 	df_range = lapply( names(x), function(id){
 
-		vc = x[[id]]$voom_curve
-
-		with(vc$voom.xy, data.frame(range(x),range(y)))
+		with(x[[id]]$geneExpr$voom.xy, data.frame(range(x),range(y)))
 	})
 	df_range = do.call(rbind, df_range)
 
 	xlim = range(df_range$range.x.)
 	ylim = range(df_range$range.y.)
-
-	# xlab = vc$voom.xy$xlab
-	# ylab = vc$voom.xy$ylab
 
 	xlab = bquote(log[2](counts + 0.5))
 	ylab = bquote(sqrt(standard~deviation))
@@ -42,10 +37,8 @@ plotVoom = function( x, ncol=3){
 	# make list of plots
 	figList = lapply( names(x), function(id){
 
-		vc = x[[id]]$voom_curve
-
-		df = with(vc$voom.xy, data.frame(x,y))
-		df_curve = with(vc$voom.line, data.frame(x,y))
+		df = with(x[[id]]$geneExpr$voom.xy, data.frame(x,y))
+		df_curve = with(x[[id]]$geneExpr$voom.line, data.frame(x,y))
 
 		ggplot(df, aes(x, y)) + geom_point(size=0.1) + geom_line(data=df_curve, aes(x,y), color="red") + theme_classic() + theme(aspect.ratio=1, plot.title = element_text(hjust = 0.5)) + xlab(xlab) + ylab(ylab) + ggtitle(id) + xlim(xlim) + ylim(ylim)
 	})
