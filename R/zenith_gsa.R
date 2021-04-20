77d60c24c23313d11a3fb7c973ad73058cc14494
Gabriel Hoffman <gabrielhoffman@glia.local>
2021-04-20 12:34:18 -0400

    add zenith_gsa

---
+++ R/zenith_gsa.R
@@ -0,0 +1,45 @@
+# Gabriel Hoffman
+# April 20, 2021
+# 
+# Apply zenith to dreamlet result
+
+#' zenith gene set analysis on dreamlet results
+#' 
+#' zenith gene set analysis on dreamlet results
+#'
+#' @param res.dl results from \code{dreamlet}
+#' @param geneSets a list of gene sets
+#' @param n_genes_min minumum number of genes in a geneset
+#'  
+#' @importFrom limma ids2indices
+#' @importFrom zenith zenith
+#' @export
+zenith_gsa = function(res.dl, geneSets, n_genes_min = 10){
+
+	# for each assay
+	df_zenith = lapply( names(res.dl), function(assay){
+
+		# extract dream fit
+		fit = res.dl[[assay]]
+
+		# Map from Ensembl genes in geneSets_GO to 
+		# from trimmed Ensembl names from RNA-seq data 
+		index = ids2indices( geneSets, rownames(fit))
+		   
+		# filter by size of gene set
+		index = index[sapply(index, length) >= n_genes_min]
+
+		# run zenith on dream fits
+		df_res = zenith(fit, coef, index)
+
+		data.frame(Assay = assay, Geneset = rownames(df_res), df_res)
+		})
+	names(df_zenith) = names(res.dl)
+	df_zenith = do.call(rbind, df_zenith)
+	rownames(df_zenith) = c()
+
+	# Compute FDR using p-values from all assays
+	df_zenith$FDR = p.adjust(df_zenith$PValue, "BH")
+
+	df_zenith
+}
