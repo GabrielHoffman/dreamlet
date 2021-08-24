871d74c6e7eacf3173adc42a00e792e57de3dde8
Gabriel Hoffman <gabriel.hoffman@gmail.com>
2021-08-24 19:41:59 -0400

    0.0.4

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,6 @@
+# dreamlet 0.0.4
+	- add aggregateToPseudoBulk() for faster access to SingleCellExperiment() backed by H5AD
+
 # dreamlet 0.0.3
 	- add argument to `processAssays()` to include extra meta-data	
 	- add subseting with assay() for dreamletProcessedData
---
+++ inst/unitTests/test_pseudobulk.R
@@ -0,0 +1,56 @@
+
+
+
+library(RUnit)
+
+# Check dreamlet:::summarizeAssayByGroup2()
+# compared to scuttle::summarizeAssayByGroup()
+test_pseudobulk_example = function(){
+                
+	example_sce <- scuttle::mockSCE()
+
+	ids <- sample(LETTERS[1:5], ncol(example_sce), replace=TRUE)
+
+	out <- scuttle::summarizeAssayByGroup(example_sce, ids)
+
+	out2 <- dreamlet:::summarizeAssayByGroup2(example_sce, ids)
+
+	checkEquals(out, out2)
+}
+
+
+# Check dreamlet::aggregateToPseudoBulk()
+# compared to muscat::aggregateData
+test_aggregateData = function(){
+
+	library(muscat)
+
+	# pseudobulk counts by cluster-sample
+	data(example_sce)
+	pb <- aggregateData(example_sce)
+
+	library(SingleCellExperiment)
+	assayNames(example_sce)  # one sheet per cluster
+	head(assay(example_sce)) # n_genes x n_samples
+
+	# scaled CPM
+	cpm <- edgeR::cpm(assay(example_sce))
+	assays(example_sce)$cpm <- cpm
+	pb <- aggregateData(example_sce, assay = "cpm", scale = TRUE)
+	# head(assay(pb)) 
+
+	pb2 <- dreamlet::aggregateToPseudoBulk(example_sce, assay = "cpm", scale = TRUE)
+	
+	check1 = checkEquals(pb, pb2)
+
+
+	# aggregate by cluster only
+	pb <- muscat::aggregateData(example_sce, by = "cluster_id")
+	length(assays(pb)) # single assay
+	head(assay(pb))    # n_genes x n_clusters
+
+	pb2 <- dreamlet::aggregateToPseudoBulk(example_sce, by = "cluster_id")
+	
+
+	check1 & checkEquals(pb, pb2)
+}
