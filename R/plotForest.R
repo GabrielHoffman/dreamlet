53a556f3d9b6873387bc919f5a0b3e055065a61d
Gabriel Hoffman <gabriel.hoffman@gmail.com>
2022-05-20 16:19:15 -0400

    update

--- R/plotForest.R
+++ R/plotForest.R
@@ -56,14 +56,12 @@ setMethod("plotForest", signature(x="dreamletResult"),
 	# Pass R CMD check
 	Assay = logFC = FDR = se = NULL
 
-	df = lapply( names(x), function(assay){
-		tab = topTable(x[[assay]], coef=coef, number=Inf)
-		data.frame(Assay = assay, Gene = rownames(tab), tab, se = with(tab, logFC / t))
-		})
-	df = do.call(rbind, df)
+ 	df = topTable(x, coef=coef, number=Inf)
+ 	df$se = with(df, logFC / t)
 	df$FDR = p.adjust(df$P.Value)
+	df = as.data.frame(df)
 
-	ggplot(df[df$Gene == gene, ], aes(Assay, logFC,  color=-log10(pmax(1e-4,FDR)) )) + geom_point() + geom_errorbar(aes(ymin = logFC - 1.96*se, ymax = logFC + 1.96*se), width=0.1) + theme_classic() + theme(plot.title = element_text(hjust = 0.5)) + coord_flip() + ggtitle(gene) + ylab(bquote(log[2]~fold~change)) + geom_hline(yintercept=0, linetype="dashed") + scale_color_gradient(name = bquote(-log[10]~FDR), low="grey", high="red", limits=c(0, 4)) + xlab('')
+	ggplot(df[df$ID == gene, ], aes(assay, logFC,  color=-log10(pmax(1e-4,FDR)) )) + geom_point() + geom_errorbar(aes(ymin = logFC - 1.96*se, ymax = logFC + 1.96*se), width=0.1) + theme_classic() + theme(plot.title = element_text(hjust = 0.5)) + coord_flip() + ggtitle(gene) + ylab(bquote(log[2]~fold~change)) + geom_hline(yintercept=0, linetype="dashed") + scale_color_gradient(name = bquote(-log[10]~FDR), low="grey", high="red", limits=c(0, 4)) + xlab('')
 })
 
 
