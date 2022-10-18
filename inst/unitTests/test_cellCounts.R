36bbddceb2ac4c78b3d0c116aa7b36da9a60924f
Gabriel Hoffman <gabriel.hoffman@gmail.com>
2022-10-17 22:02:09 -0400

    0.0.49

---
+++ inst/unitTests/test_cellCounts.R
@@ -0,0 +1,23 @@
+
+library(RUnit)
+
+test_cellCounts = function(){
+
+	library(muscat)
+	library(SingleCellExperiment)
+
+	data(example_sce)
+
+	# create pseudobulk for each sample and cell cluster
+	pb <- aggregateToPseudoBulk(example_sce, 
+	   assay = "counts",    
+	   cluster_id = 'cluster_id', 
+	   sample_id = 'sample_id',
+	   verbose=FALSE)
+
+	counts1 = cellCounts(pb)
+
+	counts2 = computeCellCounts(example_sce, 'cluster_id', 'sample_id')
+
+	checkEquals(counts1, counts2)
+}
