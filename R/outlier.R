97bac378ae739f52b3859c84c2f402cf55422e7d
Gabriel Hoffman <gabriel.hoffman@gmail.com>
2024-01-25 20:38:44 -0500

    add outliers code

--- R/outlier.R
+++ R/outlier.R
@@ -3,7 +3,7 @@
 #' Detect multivariante outliers using Mahalanobis distance using mean and covariance estimated either with standard or robust methods.
 #'
 #' @param data matrix of data
-#' @param robust use robust methods, defaults to \code{FALSE}
+#' @param robust use robust covariance method, defaults to \code{FALSE}
 #' @param ... arguments passed to \code{MASS::cov.rob()}
 #'
 #' @details
@@ -48,3 +48,73 @@ outlier <- function(data, robust = FALSE, ...) {
 
 
  
+#' Outlier analysis for each assay
+#' 
+#' Compute outlier score for each sample in each assay using \code{outlier()}
+#' 
+#' @param object \code{dreamletProcessedData} from \code{processAssays()}
+#' @param assays assays / cell types to analyze
+#' @param nPC number of PCs to uses for outlier score with \code{outlier()}
+#' @param robust use robust covariance method, defaults to \code{FALSE}
+#' @param ... arguments passed to \code{MASS::cov.rob()}
+#' 
+#' @return
+#' \itemize{
+#'  \item{\code{ID}:}{sample identifier}
+#'  \item{\code{assay}:}{specify assay}
+#'  \item{\code{PCs}:}{principal components}
+#'  \item{\code{chisq}:}{mahalanobis distance that is distributed as chisq(k) k = nPC if the data is multivariate gaussian}
+#'  \item{\code{z}:}{z-score corresponding to the chisq distance}
+#' }
+#'
+#' @examples
+#' library(muscat)
+#' library(SingleCellExperiment)
+#'
+#' data(example_sce)
+#'
+#' # create pseudobulk for each sample and cell cluster
+#' pb <- aggregateToPseudoBulk(example_sce,
+#'   assay = "counts",
+#'   cluster_id = "cluster_id",
+#'   sample_id = "sample_id",
+#'   verbose = FALSE
+#' )
+#'
+#' # voom-style normalization
+#' res.proc <- processAssays(pb, ~group_id)
+#' 
+#' # Compute PCs and outlier scores
+#' outlierByAssay( res.proc, c("B cells", "CD14+ Monocytes"))
+#
+#' @importFrom irlba prcomp_irlba
+#' @seealso \code{outlier()}
+#' @export
+outlierByAssay = function(object, assays, nPC=2, robust = FALSE, ...){
+
+  stopifnot(is(object, "dreamletProcessedData"))
+
+   df = lapply(assays, function(id){
+    # get normalized expression
+    Y <- assay(object, id)$E
+
+    # PCA
+    # dcmp <- prcomp(scale(t(Y)))
+    if( nPC < min(dim(Y))/2 ){
+      dcmp = prcomp_irlba(t(Y), n = nPC, center=TRUE, scale.=TRUE)
+    }else{
+      dcmp = prcomp(t(Y), center=TRUE, scale.=TRUE)
+    }
+
+    # outlier analysis on first 2 PCs
+    df_outlier <- outlier(dcmp$x[,seq(nPC)] * dcmp$sdev[seq(nPC)], robust = robust, ...)
+   
+    data.frame(ID = colnames(Y), 
+      assay = id, 
+      dcmp$x, 
+      df_outlier) %>%
+      as_tibble 
+  })
+  bind_rows(df)
+}
+
