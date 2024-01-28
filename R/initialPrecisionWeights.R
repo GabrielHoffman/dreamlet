782b7cb4c725be1a117d5d2068cdf57c17154b06
Gabriel Hoffman <gabriel.hoffman@gmail.com>
2024-01-28 11:33:04 -0500

    update

--- R/initialPrecisionWeights.R
+++ R/initialPrecisionWeights.R
@@ -153,7 +153,8 @@ getVarList <- function(sce, sample_id, cluster_id, shrink = TRUE, prior.count = 
     df <- df %>%
       mutate(count.gene = count.gene + 1e-4) %>%
       mutate(vif = (1 + ncell * sigSq.hat * zeta / count.gene)) %>%
-      mutate(vhat = 1 / count.gene * vif)
+      mutate(vhat = 1 / count.gene * vif) %>%
+      mutate( assay = CT)
 
     # Vhat
     matVhat <- sparseMatrix(df$Gene, df$ID,
