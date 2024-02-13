0b537aec7202ebd9f0912f5f1004f47036327154
Gabriel Hoffman <gabriel.hoffman@gmail.com>
2024-02-13 14:50:03 -0500

    1.1.17

--- R/plotPCA.R
+++ R/plotPCA.R
@@ -16,6 +16,7 @@ setGeneric("plotPCA", BiocGenerics::plotPCA)
 #' @param maxOutlierZ cap outlier z-scores at this value for plotting to maintain consistent color scale
 #' @param nrow number of rows in plot
 #' @param size size passed to \code{geom_point()}
+#' @param fdr.cutoff FDR cutoff to determine outlier
 #' 
 #' @name plotPCA
 #' @rdname plotPCA
@@ -42,27 +43,24 @@ setGeneric("plotPCA", BiocGenerics::plotPCA)
 #
 #' @seealso \code{outlierByAssay()}
 #' @export
-setMethod("plotPCA", signature(object="dreamletProcessedData"), function(object, assays = assayNames(object), nPC=2, robust = FALSE, ..., maxOutlierZ=20, nrow=2, size=2){
+setMethod("plotPCA", signature(object="dreamletProcessedData"), function(object, assays = assayNames(object), nPC=2, robust = FALSE, ..., maxOutlierZ=20, nrow=2, size=2, fdr.cutoff=0.05){
 
   stopifnot(all(assays %in% assayNames(object)))
 
   PC1 <- PC2 <- z <- pValue <- FDR <- NULL
 
-  outlierByAssay( object, assays, robust = robust, ...) %>%
+  outlierByAssay( object, assays, robust = robust, nPC=nPC,...) %>%
     tibble(FDR = p.adjust(pValue, "fdr")) %>%
     arrange(FDR) %>%
     ggplot(aes(PC1, PC2, 
       color = pmin(z, maxOutlierZ),
-      shape = FDR < 0.05)) +
+      shape = FDR < fdr.cutoff)) +
       geom_point(size=size) +
       theme_classic() +
       theme(aspect.ratio=1, plot.title = element_text(hjust = 0.5)) +
       scale_color_gradient("Outlier z", limits=c(0, maxOutlierZ), low="grey60", high="red") +
+      scale_shape_discrete(bquote(FDR < .(fdr.cutoff))) +
       facet_wrap( ~ assay, nrow=nrow, scales="free")
 })
-
-
-
-
 
 
