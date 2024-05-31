cf0bf3155e227de8defe16657d279e429d627fd2
Gabriel Hoffman <gabriel.hoffman@gmail.com>
2024-05-31 14:35:31 -0400

    clean up

--- inst/unitTests/test_pbWeights.R
+++ inst/unitTests/test_pbWeights.R
@@ -3,56 +3,56 @@
 
 
 
-test_pbWeights = function(){
+# test_pbWeights = function(){
 
-	library(muscat)
-	library(dreamlet)
+# 	library(muscat)
+# 	library(dreamlet)
 
-	data(example_sce)
+# 	data(example_sce)
 
-	# create pseudobulk for each sample and cell cluster
-	pb <- aggregateToPseudoBulk(example_sce,
-	  assay = "counts",
-	  sample_id = "sample_id",
-	  cluster_id = "cluster_id",
-	  verbose = FALSE
-	)
+# 	# create pseudobulk for each sample and cell cluster
+# 	pb <- aggregateToPseudoBulk(example_sce,
+# 	  assay = "counts",
+# 	  sample_id = "sample_id",
+# 	  cluster_id = "cluster_id",
+# 	  verbose = FALSE
+# 	)
 
-	# cell count weights
-	####################
-	weightsList <- pbWeights(example_sce,
-	    sample_id = "sample_id",
-	    cluster_id = "cluster_id")
+# 	# cell count weights
+# 	####################
+# 	weightsList <- pbWeights(example_sce,
+# 	    sample_id = "sample_id",
+# 	    cluster_id = "cluster_id")
 
-	# use externallly comptuted cell weights
-	res.proc1 <- processAssays(pb, ~ group_id, weightsList = weightsList)
+# 	# use externallly comptuted cell weights
+# 	res.proc1 <- processAssays(pb, ~ group_id, weightsList = weightsList)
 
-	# use internally comptuted cell weights
-	res.proc2 <- processAssays(pb, ~ group_id)
+# 	# use internally comptuted cell weights
+# 	res.proc2 <- processAssays(pb, ~ group_id)
 
-	for( i in seq(length(weightsList)) ){
-		weightsList[[i]][] = 1
-	}
-	res.proc3 <- processAssays(pb, ~ group_id, weightsList = weightsList)
+# 	for( i in seq(length(weightsList)) ){
+# 		weightsList[[i]][] = 1
+# 	}
+# 	res.proc3 <- processAssays(pb, ~ group_id, weightsList = weightsList)
 
-	# These should be identical, since weights of 1 are used
-	checkIdentical(res.proc3, res.proc2)
+# 	# These should be identical, since weights of 1 are used
+# 	checkIdentical(res.proc3, res.proc2)
 
-	# delta weights
-	###############
-	weightsList <- pbWeights(example_sce,
-	    sample_id = "sample_id",
-	    cluster_id = "cluster_id", 
-	    method = "delta")
+# 	# delta weights
+# 	###############
+# 	weightsList <- pbWeights(example_sce,
+# 	    sample_id = "sample_id",
+# 	    cluster_id = "cluster_id", 
+# 	    method = "delta")
 
-	# use externallly comptuted cell weights
-	res.proc1 <- processAssays(pb, ~ group_id, weightsList = weightsList)
+# 	# use externallly comptuted cell weights
+# 	res.proc1 <- processAssays(pb, ~ group_id, weightsList = weightsList)
 
-	# use internally comptuted cell weights
-	res.proc2 <- processAssays(pb, ~ group_id)
+# 	# use internally comptuted cell weights
+# 	res.proc2 <- processAssays(pb, ~ group_id)
 
-	# These should NOOOT be identical
-	res = any(assay(res.proc1, 1)$weights == assay(res.proc2, 1)$weights)
+# 	# These should NOOOT be identical
+# 	res = any(assay(res.proc1, 1)$weights == assay(res.proc2, 1)$weights)
 
-	checkIdentical(res, FALSE)
-}
+# 	checkIdentical(res, FALSE)
+# }
--- inst/unitTests/test_rowWeightedVarsMatrix.R
+++ inst/unitTests/test_rowWeightedVarsMatrix.R
@@ -1,27 +1,27 @@
 
 
 
-test_rowWeightedVarsMatrix = function(){
+# test_rowWeightedVarsMatrix = function(){
 		
-	set.seed(1)
-	n = 100
-	p = 200
-	X = matrix(rnorm(n*p), n, p)
-	W = matrix(rgamma(n*p, 14, 1), n, p)
+# 	set.seed(1)
+# 	n = 100
+# 	p = 200
+# 	X = matrix(rnorm(n*p), n, p)
+# 	W = matrix(rgamma(n*p, 14, 1), n, p)
 
-	# adapted from modi::weighted.var()0
-	# placed here to avoid importing the whole package
-	wv = function(x, w){
-		w <- w * length(w)/sum(w)
-    	sum(w * (x - weighted.mean(x, w))^2)/(sum(w) - 1)
-	}
+# 	# adapted from modi::weighted.var()0
+# 	# placed here to avoid importing the whole package
+# 	wv = function(x, w){
+# 		w <- w * length(w)/sum(w)
+#     	sum(w * (x - weighted.mean(x, w))^2)/(sum(w) - 1)
+# 	}
 
-	res1 <- sapply(seq(n), function(i) wv(X[i,], W[i,]))
+# 	res1 <- sapply(seq(n), function(i) wv(X[i,], W[i,]))
 
-	res2 <- dreamlet:::rowWeightedVarsMatrix(X, W)
+# 	res2 <- dreamlet:::rowWeightedVarsMatrix(X, W)
 
-	checkEqualsNumeric(res1, res2)
-}
+# 	checkEqualsNumeric(res1, res2)
+# }
 
 
 
